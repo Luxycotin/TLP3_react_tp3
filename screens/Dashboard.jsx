5ab8df0d2f3c1a10b5b070181ea9371274b12161
Luca Espinoza <luca.g.esp2005@gmail.com>
2025-06-02 09:04:23 -0300

    ...

--- screens/Dashboard.jsx
+++ screens/Dashboard.jsx
@@ -13,6 +13,6 @@ export default function Dashboard() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#000', // fondo negro para mantener consistencia
+    backgroundColor: '#000',
   },
 });
